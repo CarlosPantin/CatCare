7af88ed96ddcd796cad686f0b25add53cdb01a18
CarlosPantin <carlospantinpang@outlook.com>
2025-02-01 22:10:08 +0200

    test

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,8 +15,8 @@ const LoginForm = () => {
 
     const API_BASE_URL =
       process.env.NODE_ENV === "production"
-        ? process.env.REACT_APP_API_BASE_URL_PRODUCTION
-        : process.env.REACT_APP_API_BASE_URL;
+        ? "https://catcare-clak.onrender.com"
+        : "http://localhost:5000";
     console.log("NODE_ENV:", process.env.NODE_ENV);
     console.log("API BASE URL:", API_BASE_URL);
     try {
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -15,8 +15,8 @@ const Register = () => {
 
     const API_BASE_URL =
       process.env.NODE_ENV === "production"
-        ? process.env.REACT_APP_API_BASE_URL_PRODUCTION
-        : process.env.REACT_APP_API_BASE_URL;
+        ? "https://catcare-clak.onrender.com"
+        : "http://localhost:5000";
 
     try {
       const response = await axios.post(`${API_BASE_URL}/api/users/login`, {
